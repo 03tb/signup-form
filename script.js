4766862331df9fb678f72988e86d160a621b2d8b
03tb <132428099+03tb@users.noreply.github.com>
2024-01-01 22:35:52 +1100

    Password validation complete - need to add regex patterns

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const userPassword = document.querySelector('#user-password');
 const confirmPassword = document.querySelector('#confirm-password');
+const inputGroup = document.querySelector('.password-grouping')
 
 const createAccount = document.querySelector('.createBtn');
 
@@ -7,28 +8,61 @@ createAccount.addEventListener('click', () => {
     let savePassword = userPassword.value;
     let checkSavePassword = confirmPassword.value;
 
-    if (checkPasswordEmpty(userPassword, confirmPassword)) {
+    if (checkPasswordEmpty(userPassword.value, confirmPassword.value)) {
         checkPassword(savePassword, checkSavePassword);
     }
-})
+});
 
 function checkPasswordEmpty(userPassword, confirmPassword) {
-    if (userPassword == '' || confirmPassword == '') {
-        console.log('password cannot be empty')
+    const existingError = inputGroup.querySelector('.password-empty-error');
+    if (existingError) {
+        inputGroup.removeChild(existingError);
+    }
+
+    if (userPassword === '' || confirmPassword === '') {
+        const passwordEmptyError = document.createElement('div')
+        passwordEmptyError.textContent = '* Password cannot be empty';
+        passwordEmptyError.className = 'password-empty-error';
+        passwordEmptyError.setAttribute ('style', 'margin: 0; padding: 0; font-size: 10px; color: red;');
+        shakeScreen();
+        inputGroup.appendChild(passwordEmptyError)
         return false;
     }
+    return true;
 }
 
 function checkPassword(savePassword, checkSavePassword) {
     if (savePassword.length >= 8) {
         if (savePassword === checkSavePassword) {
-            console.log('passwords match')
+            alert('Account created')
         } else {
-            console.log('passwords do not match')
+            const passwordEmptyError = document.createElement('div')
+            passwordEmptyError.textContent = '* Password does not match';
+            passwordEmptyError.className = 'password-empty-error';
+            passwordEmptyError.setAttribute ('style', 'margin: 0; padding: 0; font-size: 10px; color: red;');
+            shakeScreen();
+            inputGroup.appendChild(passwordEmptyError)
+            return false;
         }
-        console.log('password length is greater than or equal to 8')
     } else {
-        console.log('Password is not long enough')
-        console.log('Password does not match')
+            const passwordEmptyError = document.createElement('div')
+            passwordEmptyError.textContent = '* Password must be 8 characters long';
+            passwordEmptyError.className = 'password-empty-error';
+            passwordEmptyError.setAttribute ('style', 'margin: 0; padding: 0; font-size: 10px; color: red;');
+            shakeScreen();
+            inputGroup.appendChild(passwordEmptyError)
     }
+}
+
+
+
+
+function shakeScreen() {
+    const container = document.querySelector('#container');
+    container.classList.add('shake');
+
+    
+    setTimeout(() => {
+        container.classList.remove('shake');
+    }, 500);
 }
